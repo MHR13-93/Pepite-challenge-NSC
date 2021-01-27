d0792abc573c6c29f8ae8caa5394122d4e3cc014
MHR13-93 <77749596+MHR13-93@users.noreply.github.com>
2021-01-27 22:16:17 +0100

    Add files via upload

---
+++ Tileset_32x32_32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Tileset_32x32_32" tilewidth="32" tileheight="32" tilecount="420" columns="15">
+ <image source="Tileset_32x32_32.png" width="490" height="898"/>
+</tileset>
